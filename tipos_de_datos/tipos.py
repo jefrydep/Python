36572471aecbae5063a739636995543c574825ba
jefry <jefrydep@gmail.com>
2023-01-29 11:00:18 -0500

    learnig python

---
+++ tipos_de_datos/tipos.py
@@ -0,0 +1,14 @@
+#tipos de datos
+
+'string'
+
+myName = 'Jefry';
+'int'
+intValue = 23;
+'boleanos'
+isPro = True;
+'float'
+flotante = 2.3;
+
+print(f'{myName,intValue,isPro,flotante}')
+
---
+++ variables.py
@@ -0,0 +1,21 @@
+my_name = 'Jefry Palomino Mayta'
+myame = 'tito'
+
+#camelcase
+#uperccase
+#snakecase es la manera recomendad de escribir variables en python
+
+number1 = 10
+# number1 += 1
+
+#concatenamos con f string
+saludo= f'Bienvenido :{myame},como estas'
+
+# del saludo  :para elemiminar datos que estan alojados en lamemoria
+# print(my_name)
+print(number1)
+print(saludo)
+
+#operadores de pertenencia
+print('como estas' in saludo) #busca datos y devuelve un valor boleano
+print('como estas'not in saludo) #busca datos que no estas devuelve un valor boleano
